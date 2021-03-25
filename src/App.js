cc9d5fdda82dbd600cd8fd9b4da6f522eda4bb9e
dgreeott <48890665+dgreeott@users.noreply.github.com>
2021-03-25 14:58:27 -0600

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./components/Navbar/Navbar";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          <Navbar />
+          <div className="container-fluid">
+            <Route path="/" exact component={TodosList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
+          </div>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar/ListItems.js
@@ -0,0 +1,17 @@
+export const ListItems = [
+    {
+      title: "Todos",
+      url: "/",
+      cName: "nav-links",
+    },
+    {
+      title: "Create Todo",
+      url: "/create",
+      cName: "nav-links",
+    },
+    {
+      title: "Edit Todo",
+      url: "/edit/:id",
+      cName: "nav-links",
+    },
+  ];
